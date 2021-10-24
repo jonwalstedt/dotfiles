f8f0bb026c3a51c2575d2c664f4bcb5229597373
Jon Wålstedt <jon.walstedt@king.com>
2021-10-24 09:01:43 +0200

    readd cmp capabilities

--- config/nvim/lua/plugin/nvim-cmp.lua
+++ config/nvim/lua/plugin/nvim-cmp.lua
@@ -1,32 +1,30 @@
 local cmp = require('cmp');
 
-print('setting up cmp')
 cmp.setup {
-  snippet = { expand = function(args) vim.fn["vsnip#anonymous"](args.body) end },
-  completion = { completeopt = 'menu,menuone,noinsert' },
-  mapping = {
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<Esc>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm(
-      { behavior = cmp.ConfirmBehavior.Replace, select = true }
-    ),
-    ['<Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end
-  },
-  sources = {
-    { name = 'nvim_lua' },
-    { name = 'nvim_lsp', priority = 9001 },
-    { name = 'path' },
-    { name = 'vsnip' },
-    { name = 'buffer', priority = 2, keyword_length = 5, max_item_count = 10 },
-    { name = 'calc' }
-  }
+    snippet = {expand = function(args) vim.fn["vsnip#anonymous"](args.body) end},
+    completion = {completeopt = 'menu,menuone,noinsert'},
+    mapping = {
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<Esc>'] = cmp.mapping.close(),
+        ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true
+        }),
+        ['<Tab>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end
+    },
+    sources = {
+        {name = 'nvim_lua'}, {name = 'nvim_lsp', priority = 9001},
+        {name = 'path'}, {name = 'vsnip'},
+        {name = 'buffer', priority = 2, keyword_length = 5, max_item_count = 10},
+        {name = 'calc'}
+    }
 }
--- config/nvim/lua/plugin/nvim-lspconfig/init.lua
+++ config/nvim/lua/plugin/nvim-lspconfig/init.lua
@@ -5,18 +5,15 @@ local palette = U.palette
 
 -- Customize diagnostics signs
 local function set_sign(type, icon)
-  local sign = string.format("LspDiagnosticsSign%s", type)
-  local texthl = string.format("LspDiagnosticsDefault%s", type)
-  vim.fn.sign_define(sign, { text = icon, texthl = texthl })
+    local sign = string.format("LspDiagnosticsSign%s", type)
+    local texthl = string.format("LspDiagnosticsDefault%s", type)
+    vim.fn.sign_define(sign, {text = icon, texthl = texthl})
 end
 
 -- Customize diagnostics highlights
 local function set_highlight(type, color)
-  vim.cmd(
-    string.format(
-      "highlight! LspDiagnosticsDefault%s guifg=%s", type, color
-    )
-  )
+    vim.cmd(string.format("highlight! LspDiagnosticsDefault%s guifg=%s", type,
+                          color))
 end
 
 -- Check if a eslint config is present
@@ -35,13 +32,10 @@ end
 -- end
 
 _G.formatting = function()
-  if not vim.g[string.format("format_disabled_%s", vim.bo.filetype)] then
-    vim.lsp.buf.formatting(
-      vim.g[string.format(
-        "format_options_%s", vim.bo.filetype
-      )] or {}
-    )
-  end
+    if not vim.g[string.format("format_disabled_%s", vim.bo.filetype)] then
+        vim.lsp.buf.formatting(vim.g[string.format("format_options_%s",
+                                                   vim.bo.filetype)] or {})
+    end
 end
 
 set_sign("Hint", "")
@@ -54,169 +48,160 @@ set_highlight("Information", palette.cyan)
 set_highlight("Warning", palette.yellow)
 set_highlight("Error", palette.red)
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
--- local capabilities = require('cmp_nvim_lsp').update_capabilities(
---   vim.lsp.protocol.make_client_capabilities()
--- )
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp
+                                                                     .protocol
+                                                                     .make_client_capabilities())
 
 -- local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities.textDocument.completion.completionItem.resolveSupport = {
-  properties = { 'documentation', 'detail', 'additionalTextEdits' }
+    properties = {'documentation', 'detail', 'additionalTextEdits'}
 }
 
 -- configuring diagnostics
-vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    underline = true,
-    virtual_text = false, -- disable inline diagnostics
-    signs = true,
-    update_in_insert = false
-  }
-)
+vim.lsp.handlers['textDocument/publishDiagnostics'] =
+    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+        underline = true,
+        virtual_text = false, -- disable inline diagnostics
+        signs = true,
+        update_in_insert = false
+    })
 
 local function setup_servers()
 
-  -- Efm language server
-  -- https://github.com/mattn/efm-langserver
-  lspconfig.efm.setup {
-    on_attach = function(client)
-      client.resolved_capabilities.document_formatting = true
-      on_attach_common(client)
-    end,
-    init_options = {
-      documentFormatting = true,
-      codeAction = true,
-      document_formatting = true
-    },
-    root_dir = lspconfig.util.root_pattern({ '.git/' }),
+    -- Efm language server
+    -- https://github.com/mattn/efm-langserver
+    lspconfig.efm.setup {
+        on_attach = function(client)
+            client.resolved_capabilities.document_formatting = true
+            on_attach_common(client)
+        end,
+        init_options = {
+            documentFormatting = true,
+            codeAction = true,
+            document_formatting = true
+        },
+        root_dir = lspconfig.util.root_pattern({'.git/'}),
 
-    filetypes = {
-      "javascript",
-      "javascriptreact",
-      "javascript.jsx",
-      "typescript",
-      "typescript.tsx",
-      "typescriptreact",
-      "less",
-      "scss",
-      "css",
-      "lua"
-    },
-    settings = { languages = languages, log_level = 1, log_file = '~/efm.log' }
-  }
-
-  -- Lua language server
-  -- local system_name
-  if vim.fn.has("mac") == 1 then
-    system_name = "macOS"
-  elseif vim.fn.has("unix") == 1 then
-    system_name = "Linux"
-  elseif vim.fn.has('win32') == 1 then
-    system_name = "Windows"
-  else
-    print("Unsupported system for sumneko")
-  end
-
-  local sumneko_root_path =
-    '/Users/jonwalstedt/repos/language-servers/lua/lua-language-server'
-  local sumneko_binary = sumneko_root_path .. "/bin/" .. system_name ..
-                           "/lua-language-server"
-  lspconfig.sumneko_lua.setup(
-    {
-      cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
-      settings = {
-        Lua = { diagnostics = { enable = true, globals = { "vim" } } }
-      },
-
-      on_attach = on_attach_common
+        filetypes = {
+            "javascript", "javascriptreact", "javascript.jsx", "typescript",
+            "typescript.tsx", "typescriptreact", "less", "scss", "css", "lua"
+        },
+        settings = {
+            languages = languages,
+            log_level = 1,
+            log_file = '~/efm.log'
+        }
     }
-  )
 
-  lspconfig.eslint.setup {
-    capabilities = capabilities,
-    filetypes = {},
-    on_attach = function(client)
-      if client.config.flags then
-        client.config.flags.allow_incremental_sync = true
-      end
-      client.resolved_capabilities.document_formatting = true
-      on_attach_common(client)
-    end,
-    settings = {}
-  }
-
-  -- Typescript languageserver
-  -- https://github.com/theia-ide/typescript-language-server
-  lspconfig.tsserver.setup {
-    capabilities = capabilities,
-    on_attach = function(client)
-      if client.config.flags then
-        client.config.flags.allow_incremental_sync = true
-      end
-      client.resolved_capabilities.document_formatting = false
-      on_attach_common(client)
+    -- Lua language server
+    -- local system_name
+    if vim.fn.has("mac") == 1 then
+        system_name = "macOS"
+    elseif vim.fn.has("unix") == 1 then
+        system_name = "Linux"
+    elseif vim.fn.has('win32') == 1 then
+        system_name = "Windows"
+    else
+        print("Unsupported system for sumneko")
     end
-  }
 
-  -- Go languageserver
-  -- https://github.com/golang/tools/tree/master/gopls
-  lspconfig.gopls.setup {
-    on_attach = function(client)
-      client.resolved_capabilities.document_formatting = false
-      on_attach_common(client)
-    end
-  }
+    local sumneko_root_path =
+        '/Users/jonwalstedt/repos/language-servers/lua/lua-language-server'
+    local sumneko_binary = sumneko_root_path .. "/bin/" .. system_name ..
+                               "/lua-language-server"
+    lspconfig.sumneko_lua.setup({
+        cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
+        settings = {Lua = {diagnostics = {enable = true, globals = {"vim"}}}},
 
-  -- Python languageserver
-  -- https://github.com/palantir/python-language-server
-  -- lspconfig.pyls.setup {
-  --   on_attach = on_attach_common,
-  --   settings = {
-  --     pyls = {
-  --       plugins = {
-  --         pycodestyle = {
-  --           enabled = false,
-  --           ignore = {
-  --             "E501"
-  --           }
-  --         }
-  --       }
-  --     }
-  --   }
-  -- }
+        on_attach = on_attach_common
+    })
 
-  lspconfig.pyright.setup { on_attach = on_attach_common }
+    lspconfig.eslint.setup {
+        capabilities = capabilities,
+        filetypes = {},
+        on_attach = function(client)
+            if client.config.flags then
+                client.config.flags.allow_incremental_sync = true
+            end
+            client.resolved_capabilities.document_formatting = true
+            on_attach_common(client)
+        end,
+        settings = {}
+    }
 
-  -- Vim languageserver
-  -- https://github.com/iamcco/vim-language-server
-  lspconfig.vimls.setup { on_attach = on_attach_common }
+    -- Typescript languageserver
+    -- https://github.com/theia-ide/typescript-language-server
+    lspconfig.tsserver.setup {
+        capabilities = capabilities,
+        on_attach = function(client)
+            if client.config.flags then
+                client.config.flags.allow_incremental_sync = true
+            end
+            client.resolved_capabilities.document_formatting = false
+            on_attach_common(client)
+        end
+    }
 
-  -- Docker languageserver
-  -- https://github.com/rcjsuen/dockerfile-language-server-nodejs
-  lspconfig.dockerls.setup { on_attach = on_attach_common }
+    -- Go languageserver
+    -- https://github.com/golang/tools/tree/master/gopls
+    lspconfig.gopls.setup {
+        on_attach = function(client)
+            client.resolved_capabilities.document_formatting = false
+            on_attach_common(client)
+        end
+    }
 
-  -- Terraform languageserver
-  -- https://github.com/hashicorp/terraform-ls
-  lspconfig.terraformls.setup {
-    on_attach = on_attach_common,
-    cmd = { "terraform-ls", "serve" },
-    filetypes = { "tf" }
-  }
+    -- Python languageserver
+    -- https://github.com/palantir/python-language-server
+    -- lspconfig.pyls.setup {
+    --   on_attach = on_attach_common,
+    --   settings = {
+    --     pyls = {
+    --       plugins = {
+    --         pycodestyle = {
+    --           enabled = false,
+    --           ignore = {
+    --             "E501"
+    --           }
+    --         }
+    --       }
+    --     }
+    --   }
+    -- }
 
-  -- CSS languageserver
-  -- https://github.com/vscode-langservers/vscode-css-languageserver-bin
-  lspconfig.cssls.setup {
-    on_attach = on_attach_common,
-    capabilities = capabilities,
-    cmd = { "html-languageserver", "--stdio" },
-    filetypes = { "html" },
-    init_options = {
-      configurationSection = { "html", "css", "javascript" },
-      embeddedLanguages = { css = true, javascript = true }
-    },
-    settings = {}
-  }
+    lspconfig.pyright.setup {on_attach = on_attach_common}
+
+    -- Vim languageserver
+    -- https://github.com/iamcco/vim-language-server
+    lspconfig.vimls.setup {on_attach = on_attach_common}
+
+    -- Docker languageserver
+    -- https://github.com/rcjsuen/dockerfile-language-server-nodejs
+    lspconfig.dockerls.setup {on_attach = on_attach_common}
+
+    -- Terraform languageserver
+    -- https://github.com/hashicorp/terraform-ls
+    lspconfig.terraformls.setup {
+        on_attach = on_attach_common,
+        cmd = {"terraform-ls", "serve"},
+        filetypes = {"tf"}
+    }
+
+    -- CSS languageserver
+    -- https://github.com/vscode-langservers/vscode-css-languageserver-bin
+    lspconfig.cssls.setup {
+        on_attach = on_attach_common,
+        capabilities = capabilities,
+        cmd = {"html-languageserver", "--stdio"},
+        filetypes = {"html"},
+        init_options = {
+            configurationSection = {"html", "css", "javascript"},
+            embeddedLanguages = {css = true, javascript = true}
+        },
+        settings = {}
+    }
 end
 
 setup_servers()
