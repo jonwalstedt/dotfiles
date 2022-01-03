b239fa623c18624f4a2463b1422adb025dfea959
Jon Wålstedt <jon.walstedt@king.com>
2022-01-03 13:06:11 +0100

    nvim tweaks

--- config/nvim/lua/core/commands.lua
+++ config/nvim/lua/core/commands.lua
@@ -1,49 +1,49 @@
 local command = U.command
 
 -- Lsp
-command("LspReload", "lua require'lsp_config'.lsp_reload(0)")
-command("LspStop", "lua require'lsp_config'.lsp_stop(0)")
-command("LspClearDiagnostics", "lua vim.lsp.diagnostic.clear(0)")
-command("LspPrint", "lua print(vim.inspect(vim.lsp.buf_get_clients()))")
+command('LspReload', "lua require'lsp_config'.lsp_reload(0)")
+command('LspStop', "lua require'lsp_config'.lsp_stop(0)")
+command('LspClearDiagnostics', 'lua vim.lsp.diagnostic.clear(0)')
+command('LspPrint', 'lua print(vim.inspect(vim.lsp.buf_get_clients()))')
 
 -- Profiling
 command(
-  "ProfileMe",
-    "profile start profile.log <bar> profile func * <bar> profile file *"
+  'ProfileMe',
+  'profile start profile.log <bar> profile func * <bar> profile file *'
 )
-command("ProfileStop", "profile pause")
+command('ProfileStop', 'profile pause')
 
 -- Misc
 -- execute shell script in file
-command("Exec", "set splitright | vnew | set filetype=sh | read !sh #")
+command('Exec', 'set splitright | vnew | set filetype=sh | read !sh #')
 -- copy buffer file path
-command("CopyBufferPath", ":let @+ = expand('%:p')")
+command('CopyBufferPath', ":let @+ = expand('%:p')")
 
 command(
-  "BD",
-    "call fzf#run(fzf#wrap({ 'source': CustomFZF#Bufs(), 'sink*': { lines -> execute('bwipeout '.join(map(lines, {_, line -> split(line)[0]}))) }, 'options': '--multi --reverse --bind ctrl-a:select-all+accept' }))"
+  'BD',
+  "call fzf#run(fzf#wrap({ 'source': CustomFZF#Bufs(), 'sink*': { lines -> execute('bwipeout '.join(map(lines, {_, line -> split(line)[0]}))) }, 'options': '--multi --reverse --bind ctrl-a:select-all+accept' }))"
 )
 command(
-  "Ag",
-    ":call fzf#vim#ag(<q-args>, {'options': '--delimiter : --nth 4..'}, <bang>0)"
+  'Ag',
+  ":call fzf#vim#ag(<q-args>, {'options': '--delimiter : --nth 4..'}, <bang>0)"
 )
 
 -- Git
-command("ToggleGStatus", "call CustomFugitive#ToggleGStatus()")
+command('ToggleGStatus', 'call CustomFugitive#ToggleGStatus()')
 command(
-  "-nargs=* Glg",
-    "Git --paginate log --graph --pretty=format:'%h - (%ad)%d %s <%an>' --abbrev-commit --date=local <args>"
+  '-nargs=* Glg',
+  "vertical Git --paginate log --graph --pretty=format:'%h - (%ad)%d %s <%an>' --abbrev-commit --date=local <args>"
 )
 command(
-  "-nargs=* GV",
-    "Git --paginate log --graph --pretty=format:'%h - (%ad)%d %s <%an>' --abbrev-commit --date=local <args>"
+  '-nargs=* GV',
+  "vertical Git --paginate log --graph --pretty=format:'%h - (%ad)%d %s <%an>' --abbrev-commit --date=local <args>"
 )
-command("GitFileHistory", "0Gclog")
-command("GFileHistory", "0Gclog")
-command("GitDiffWithHash", "call CustomFugitive#DiffFileWithHash()")
-command("GDiffWithHash", "call CustomFugitive#DiffFileWithHash()")
+command('GitFileHistory', '0Gclog')
+command('GFileHistory', '0Gclog')
+command('GitDiffWithHash', 'call CustomFugitive#DiffFileWithHash()')
+command('GDiffWithHash', 'call CustomFugitive#DiffFileWithHash()')
 
 -- Note taking
-command("NotesBrowse", ":call Notes#Browse()")
-command("NotesTODO", ":call Notes#Todo()")
-command("NotesCreate", ":call Notes#Create()")
+command('NotesBrowse', ':call Notes#Browse()')
+command('NotesTODO', ':call Notes#Todo()')
+command('NotesCreate', ':call Notes#Create()')
--- config/nvim/lua/plugin/null-ls.lua
+++ config/nvim/lua/plugin/null-ls.lua
@@ -28,9 +28,9 @@ M.setup = function()
         },
       },
       eslint,
-      -- null_ls.builtins.diagnostics.eslint,
-      -- null_ls.builtins.code_actions.eslint,
-      -- null_ls.builtins.formatting.prettier,
+      null_ls.builtins.diagnostics.eslint_d,
+      null_ls.builtins.code_actions.eslint_d,
+      --null_ls.builtins.formatting.prettierd,
 
       b.formatting.prettier.with {
         prefer_local = 'node_modules/.bin',
