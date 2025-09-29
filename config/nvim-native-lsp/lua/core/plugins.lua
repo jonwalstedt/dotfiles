560f5801570e700fba56eb22906682627d5f9157
Jon Wålstedt <jon.walstedt@mentimeter.com>
2025-09-29 15:04:38 +0200

    misc

--- config/nvim-native-lsp/lua/core/plugins.lua
+++ config/nvim-native-lsp/lua/core/plugins.lua
@@ -22,43 +22,70 @@ require('lazy').setup {
     end,
   },
   {
-    'olimorris/codecompanion.nvim',
-    dependencies = {
-      { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
-      { 'nvim-lua/plenary.nvim' },
-      -- Test with blink.cmp
-      -- {
-      --   'saghen/blink.cmp',
-      --   lazy = false,
-      --   version = '*',
-      --   dependencies = 'rafamadriz/friendly-snippets',
-      --   opts = {
-      --     menu = {
-      --       auto_show = false,
-      --     },
-      --     keymap = {
-      --       preset = 'enter',
-      --       ['<S-Tab>'] = { 'select_prev', 'fallback' },
-      --       ['<Tab>'] = { 'select_next', 'fallback' },
-      --     },
-      --     appearance = {
-      --       use_nvim_cmp_as_default = true,
-      --       nerd_font_variant = 'mono',
-      --     },
-      --     sources = {
-      --       default = { 'lsp', 'path', 'buffer', 'codecompanion' },
-      --       per_filetype = {
-      --         codecompanion = { 'codecompanion' },
-      --       },
-      --     },
-      --   },
-      -- },
-      opts_extend = { 'sources.default' },
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    version = false, -- Never set this value to "*"! Never!
+    opts = {
+      -- add any opts here
+      -- for example
+      provider = 'openai',
+      providers = {
+        openai = {
+          endpoint = 'https://api.openai.com/v1',
+          model = 'gpt-4o', -- your desired model (or use gpt-4o, etc.)
+          -- extra_request_body = {
+          --   timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+          --   temperature = 0.75,
+          --   max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+          --   --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+          -- },
+        },
+      },
     },
-    config = function()
-      require 'plugin.codecompanion'
-    end,
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = 'make',
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'echasnovski/mini.pick', -- for file_selector provider mini.pick
+      'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
+      'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+      'ibhagwan/fzf-lua', -- for file_selector provider fzf
+      'stevearc/dressing.nvim', -- for input provider dressing
+      'folke/snacks.nvim', -- for input provider snacks
+      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+      'zbirenbaum/copilot.lua', -- for providers='copilot'
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
   },
+
   -- {
   --   'pmizio/typescript-tools.nvim',
   --   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
