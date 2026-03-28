e0e3ddce8f8e7beef0e517ed11fae4ae1b39e542
Jon Wålstedt <jon.walstedt@elastic.co>
2026-03-28 11:29:00 +0100

    remove commented out plugins

--- config/nvim-native-lsp/lua/core/plugins.lua
+++ config/nvim-native-lsp/lua/core/plugins.lua
@@ -32,12 +32,6 @@ require('lazy').setup {
     end,
   },
 
-  -- {
-  --   'pmizio/typescript-tools.nvim',
-  --   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-  --   opts = {},
-  -- },
-
   -- Highlight, edit, and navigate code using a fast incremental parsing library
   {
     "nvim-treesitter/nvim-treesitter",
@@ -123,7 +117,7 @@ require('lazy').setup {
     end,
   },
 
-  { 'rose-pine/neovim', name = 'rose-pine' },
+  { 'rose-pine/neovim',       name = 'rose-pine' },
 
   {
     'folke/zen-mode.nvim',
@@ -231,20 +225,6 @@ require('lazy').setup {
 
   -- Misc
   { 'github/copilot.vim' },
-  -- {
-  --   'CopilotC-Nvim/CopilotChat.nvim',
-  --   branch = 'canary',
-  --   dependencies = {
-  --     { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
-  --     { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
-  --   },
-  --   opts = {
-  --     debug = true, -- Enable debugging
-  --     -- See Configuration section for rest
-  --   },
-  --   -- See Commands section for default commands if you want to lazy load on them
-  -- },
-
   { 'tpope/vim-surround' },
   { 'othree/html5.vim' },
   { 'pangloss/vim-javascript' },
@@ -307,11 +287,11 @@ require('lazy').setup {
       'nvim-neotest/neotest-python',
     },
     keys = {
-      { '<leader>Tt', function() require('neotest').run.run() end,                  desc = 'Run nearest test' },
+      { '<leader>Tt', function() require('neotest').run.run() end,                   desc = 'Run nearest test' },
       { '<leader>Tf', function() require('neotest').run.run(vim.fn.expand('%')) end, desc = 'Run test file' },
-      { '<leader>Tl', function() require('neotest').run.run_last() end,             desc = 'Run last test' },
-      { '<leader>Ts', function() require('neotest').summary.toggle() end,           desc = 'Toggle test summary' },
-      { '<leader>To', function() require('neotest').output_panel.toggle() end,      desc = 'Toggle output panel' },
+      { '<leader>Tl', function() require('neotest').run.run_last() end,              desc = 'Run last test' },
+      { '<leader>Ts', function() require('neotest').summary.toggle() end,            desc = 'Toggle test summary' },
+      { '<leader>To', function() require('neotest').output_panel.toggle() end,       desc = 'Toggle output panel' },
     },
     config = function()
       require('plugin.neotest')
